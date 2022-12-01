790e2535638a8b8f67059b68d1ae5a85baade218
Laurent Cocault <laurent.cocault@capgemini.com>
2022-12-01 13:14:31 +0100

    Day 1, Step 2

--- AOF-2022-01-CaloryCounting/src/main.rs
+++ AOF-2022-01-CaloryCounting/src/main.rs
@@ -1,24 +1,20 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::vec::Vec;
 
 struct Counter {
-    max: u64,
-    max_line: u32,
+    sums: Vec<u64>,
     current: u64,
 }
 
-fn process_line(line: String, line_num: u32, counter: Counter) -> Counter {
+fn process_line(line: String, counter: Counter) -> Counter {
     let mut new_counter = Counter {
-        max: counter.max,
-        max_line: counter.max_line,
+        sums: counter.sums,
         current: counter.current,
     };
     if line.is_empty() {
         // An empty line means we close the current count
-        if counter.current > counter.max {
-            new_counter.max = counter.current;
-            new_counter.max_line = line_num;
-        }
+        new_counter.sums.push(counter.current);
         new_counter.current = 0;
     } else {
         // A non-empty line means we accumulate value
@@ -32,21 +28,26 @@ fn process_input(filename: &str) {
     // File Processing
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
-    let mut line_num: u32 = 0;
     // Data management
     let mut count = Counter {
-        max: 0,
-        max_line: 0,
+        sums: Vec::new(),
         current: 0,
     };
 
     for (_index, file_line) in reader.lines().enumerate() {
         let line = file_line.unwrap();
-        line_num = line_num + 1;
-        count = process_line(line, line_num, count);
+        count = process_line(line, count);
     }
 
-    println!("Max count is {} on line {}", count.max, count.max_line);
+    // Sort and sum the top 3
+    count.sums.sort_by_key(|n| std::u64::MAX - n);
+    count.sums.truncate(3);
+    let mut sum = 0;
+    for value in count.sums.iter() {
+        sum = sum + value;
+    }
+
+    println!("Sum of top 3 calories is {}", sum);
 }
 
 fn main() {
